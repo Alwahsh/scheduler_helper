a55e570b26f0668a978fe8ff8523361924f0732e
Ahmed AbouElhamayed <alwahsh.ahmed@gmail.com>
2016-02-05 17:09:22 +0000

    finalize

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :departments
   get 'home/index'
 
   get 'home_controller/index'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103205454) do
+ActiveRecord::Schema.define(version: 20160205170519) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20151103205454) do
   end
 
   create_table "day_collections", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "departments", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "head_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
